e497a5ff585048d0e56c9afc33ab985184b942c2
kasib.kismath <Kasib.Kismath@cloudsolutions-sa.com>
2020-08-24 07:47:50 +0530

    Updated the app structure

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-let routes = require("./routes/tenant-ledger/tenant-ledger.route");
+let routes = require("./app/routes/tenant-ledger/tenant-ledger.route");
 routes(app);
 
 const port = process.env.PORT || 4000;
